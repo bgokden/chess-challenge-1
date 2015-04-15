7d6b7a3a85de54b509dd89432435d8e986ef738d
Carlos Martins <carlos.mfa.martins@gmail.com>
2015-04-16 00:37:56 +0100

    Added layout methods for testing positions

--- src/main/java/net/martins/samples/chess/ChessLayout.java
+++ src/main/java/net/martins/samples/chess/ChessLayout.java
@@ -23,23 +23,65 @@ public class ChessLayout {
 		this.pieceOffsets = new HashMap<Integer, ChessPiece>();
 	}
 	
+	/**
+	 * Place a chess piece on the board at position column, row
+	 * @param piece Chess piece to place
+	 * @param column Column position place (0 is first column)
+	 * @param row Row of the position to place (0 is first row)
+	 */
+	public void setChessPiece(ChessPiece piece, int column, int row) {
+		int offset = width * row + column;
+		if(offset > maxOffset)
+			throw new ArrayIndexOutOfBoundsException("Piece fall outside the chess board");
+		pieceOffsets.put(offset, piece);
+	}
+	
 
 	/**
-	 * Checks whether there is a chess piece in a diagonal from the specified point (defined by column, row) and up to the specified radius.
-	 * @param column Column of the starting point (0 is first column)
-	 * @param row Row of the starting point (0 is first row)
-	 * @param radius
-	 * @return
+	 * Checks whether there is a chess piece in the board in a position diagonal to the specified point (defined by column, row).
+	 * @param column Column position to check against (0 is first column)
+	 * @param row Row of the position to check against (0 is first row)
+	 * @return true if there is a piece diagonal to the specified position
 	 */
 	public boolean hasColidingDiagonal(int column, int row, int radius) {
+		for(Integer offset : pieceOffsets.keySet()) {
+			int row1 = offset.intValue() / width;
+			int column1 = offset.intValue() % width;
+			if(Math.abs(row1 - row) == Math.abs(column1 - column))
+				return true;
+		}
 		return false;
 	}
 	
-	public boolean hasColidingPrependicular(int column, int row, int radius) {
+	/**
+	 * Checks whether there is a piece in the board in a position perpendicular (same row or column) to the specified position.
+	 * @param column Column position to check against (0 is first column)
+	 * @param row Row of the position to check against (0 is first row)
+	 * @return true if there is a piece perpendicular to the specified position
+	 */
+	public boolean hasColidingPrependicular(int column, int row) {
+		for(Integer offset : pieceOffsets.keySet()) {
+			int row1 = offset.intValue() / width;
+			int column1 = offset.intValue() % width;
+			if(row1 == row || column1 == column)
+				return true;
+		}
 		return false;
 	}
 	
+	/**
+	 * Checks whether there is a piece in the board in a position that can be reached from the specified point by a knight
+	 * @param column
+	 * @param row
+	 * @return
+	 */
 	public boolean hasColidingKnightMoves(int column, int row) {
+		for(Integer offset : pieceOffsets.keySet()) {
+			int drow = Math.abs(offset.intValue() / width - row);
+			int dcolumn = Math.abs(offset.intValue() % width - column);
+			if((drow == 2 && dcolumn == 1) || (drow == 1 && dcolumn == 2))
+				return true;
+		}
 		return false;
 	}
 }
